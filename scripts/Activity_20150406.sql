6da45bec3cb05886aa33874c7c0502908cf9adad
lgarcia1 <lgarciar@psl.com.co>
2015-04-06 20:09:32 -0500

    Script of Activity 1.

---
+++ scripts/Activity_20150406.sql
@@ -0,0 +1,15 @@
+﻿/* 
+select * from comment;
+*/	
+
+with recursive nodes (id, comment, author, parent_comment_id, movie_id) as (
+		select c.id, c.comment, c.author, c.parent_comment_id, c.movie_id
+		from comment as c 
+		where c.id = 32 -- Id to find.
+		and c.movie_id = 8 -- Movie Id.
+	UNION ALL
+		select c.id, c.comment, c.author, c.parent_comment_id, c.movie_id 
+		from nodes ns
+		join comment c on c.parent_comment_id = ns.id
+)
+select * from nodes order by id asc;
